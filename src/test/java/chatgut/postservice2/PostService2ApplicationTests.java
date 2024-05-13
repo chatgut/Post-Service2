df875e0d6c1cf7fae49ac0075b0e200a210c5358
Alfred <alfredbild@gmail.com>
2024-05-13 10:10:52 +0200

    Initial commit

---
+++ src/test/java/chatgut/postservice2/PostService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package chatgut.postservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostService2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
