7fb9679ca516c8b72a610d8cea4399a5a6a7043a
Alfred <alfredbild@gmail.com>
2024-05-16 11:40:02 +0200

    Add MessageRepository

---
+++ src/main/java/chatgut/postservice2/repository/MessageRepository.java
@@ -0,0 +1,9 @@
+package chatgut.postservice2.repository;
+
+import chatgut.postservice2.entity.MessageEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<MessageEntity, String> {
+}
