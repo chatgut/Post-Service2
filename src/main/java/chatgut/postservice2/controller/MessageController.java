9cc92ab3535c4f583def735e61fd69b40d239af6
Alfred <alfredbild@gmail.com>
2024-05-17 15:46:33 +0200

    Update MessageController

--- src/main/java/chatgut/postservice2/controller/MessageController.java
+++ src/main/java/chatgut/postservice2/controller/MessageController.java
@@ -2,6 +2,7 @@ package chatgut.postservice2.controller;
 
 import chatgut.postservice2.entity.Message;
 import chatgut.postservice2.repository.MessageDAO;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +16,27 @@ public class MessageController {
     private MessageDAO messageDAO;
 
     @PostMapping
-    public ResponseEntity<Message> postMessage(@RequestBody Message message) {
-        return new ResponseEntity<>(messageDAO.postMessage(message), HttpStatus.CREATED);
+    public ResponseEntity<Message> postMessage(@RequestBody Message message,
+                                               @RequestHeader("userid") String userId) {
+        // Set senderUsername from the request header
+        message.setSenderUsername(userId);
+
+        // Set receiverUsername from the message's 'to' field
+        message.setReceiverUsername(message.getReceiverUsername());
+
+        // Save the message
+        Message savedMessage = messageDAO.postMessage(message);
+
+        // Return the saved message with HTTP status CREATED
+        return new ResponseEntity<>(savedMessage, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Message> getMessageById(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(messageDAO.getMessageById(id), HttpStatus.OK);
+        // Retrieve message by id from messageDAO
+        Message message = messageDAO.getMessageById(id);
+
+        // Return the message with HTTP status OK
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
